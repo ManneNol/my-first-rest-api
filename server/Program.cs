61c33f48611342f8e34f428e73e79c164877f3db
manne <manne@mosslightstudio.com>
2025-01-28 13:41:08 +0100

    feat: Add id to user definition
    Used for replacing user methods that require index

--- server/Program.cs
+++ server/Program.cs
@@ -2,15 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 List<User> users = new();
-users.add(new("User 1"));
-users.add(new("User 2"));
-users.add(new("User 3"));
+users.Add(new(1,"User 1"));
+users.Add(new(2,"User 2"));
+users.Add(new(3,"User 3"));
 
 app.MapGet("/users", GetUsers);
-app.MapGet("/users/{index}", GetUser);
+app.MapGet("/users/{id}", GetUser);
 app.MapPost("/users", PostUser);
+app.MapDelete("/users/{id}", DeleteUser);
 
 app.Run();
+
+async Task
+DeleteUser(int id)
+{
+    users.Remove(users.Find((user)=>user.Id == id));
+}
 
 async Task
 PostUser(User user)
@@ -24,21 +31,13 @@ GetUsers()
     return users; // return select * from users
 }
 
-async Task<User>
-GetUser(int index)
+async Task<User?>
+GetUser(int id)
 {
-    User user = users[index];
+    User? user = users.Find((user)=>user.Id==id);
+
     return user; // return select * from users where id == userId
 }
 
-public record User(string Name);
 
-async Task<string>
-UserPage(int userId)
-{
-    User user = QueryUser(userId);
-
-    return $"""
-            <p>Welcome, {user.name}!</p>
-        """
-}
+public record User(int Id, string Name);
